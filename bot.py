54186cb2fc3e46ca1932d7548578dd4a5abcd0ac
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-07-25 13:12:44 +0530

    vro

--- bot.py
+++ bot.py
@@ -16,6 +16,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+# Kang with credits vomro
+
 import os
 import logging
 from pyrogram import filters, Client, idle
